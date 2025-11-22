cfc6086a3f3dd893a55dc148ec654a0820b254d8
Hariyanto S. Auna <hariyantosauna@gmail.com>
2025-11-22 12:09:26 +0800

    kklkjasadsa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>LMS</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
